1fb7e283b87a15d731a56864ecafb9026ff1f94e
ngyuki <ngyuki.jp@gmail.com>
2017-07-15 12:38:56 +0900

    phpunit 5.7

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,5 @@ $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Test\\', __DIR__);
 $loader->add('TestHelper\\', __DIR__);
 
-require_once 'PHPUnit/Framework/Assert/Functions.php';
-
-return $loader;
+$reflection = new ReflectionClass('PHPUnit_Framework_Assert');
+require_once dirname($reflection->getFileName()) . '/Assert/Functions.php';
